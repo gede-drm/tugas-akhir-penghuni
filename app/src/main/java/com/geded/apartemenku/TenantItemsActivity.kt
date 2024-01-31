4f8d7e81bbcfe91afea9f7e2a56749ca1698aafe
gededrm <114561837+gede-drm@users.noreply.github.com>
2024-01-31 19:36:10 +0700

    Shows Tenant's Reviews

--- app/src/main/java/com/geded/apartemenku/TenantItemsActivity.kt
+++ app/src/main/java/com/geded/apartemenku/TenantItemsActivity.kt
@@ -8,22 +8,25 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.view.inputmethod.EditorInfo
+import android.widget.Button
 import android.widget.TextView
-import android.widget.Toast
 import androidx.core.view.isVisible
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.LinearLayoutManager
-import androidx.recyclerview.widget.StaggeredGridLayoutManager
+import androidx.recyclerview.widget.RecyclerView
 import com.android.volley.Response
 import com.android.volley.toolbox.StringRequest
 import com.android.volley.toolbox.Volley
 import com.geded.apartemenku.databinding.ActivityTenantItemsBinding
+import com.google.android.material.bottomsheet.BottomSheetBehavior
+import com.google.android.material.bottomsheet.BottomSheetDialog
 import com.google.android.material.dialog.MaterialAlertDialogBuilder
 import org.json.JSONObject
 
 class TenantItemsActivity : AppCompatActivity() {
     private lateinit var binding: ActivityTenantItemsBinding
     var items:ArrayList<TenantItem> = arrayListOf()
+    var tenantReviews:ArrayList<Review> = arrayListOf()
     var tenant_id = 0
     var tenant_name = ""
     var tenant_type = ""
@@ -77,12 +80,35 @@ class TenantItemsActivity : AppCompatActivity() {
             val search = binding.txtSearchTenItem.text.toString()
             getTenantItemsData(search)
         }
+
+        binding.btnTenReview.setOnClickListener {
+            if(tenantReviews.size > 0){
+                val dialog = BottomSheetDialog(this)
+                val view =layoutInflater.inflate(R.layout.bottom_sheet_ten_review, null)
+                dialog.setCancelable(false)
+                dialog.setContentView(view)
+                dialog.behavior.state = BottomSheetBehavior.STATE_EXPANDED
+                dialog.show()
+
+                val lm: LinearLayoutManager = LinearLayoutManager(this)
+                var recyclerView = view.findViewById<RecyclerView>(R.id.recViewTenReviews)
+                recyclerView.layoutManager = lm
+                recyclerView.setHasFixedSize(true)
+                recyclerView.adapter = ItemReviewAdapter(tenantReviews, this)
+                recyclerView.isVisible = true
+
+                view.findViewById<Button>(R.id.btnCloseDialogTenReview).setOnClickListener {
+                    dialog.dismiss()
+                }
+            }
+        }
     }
 
     override fun onResume() {
         super.onResume()
         val search = binding.txtSearchTenItem.text.toString()
         getTenantItemsData(search)
+        getTenantReviews()
     }
 
     fun getTenantItemsData(search:String){
@@ -154,5 +180,51 @@ class TenantItemsActivity : AppCompatActivity() {
         binding.txtEmptyItemTenList.visibility = View.GONE
         binding.progressBarTItems.visibility = View.GONE
         binding.refreshLayoutTenantItems.isRefreshing = false
+    }
+
+    fun getTenantReviews(){
+        tenantReviews.clear()
+        val q = Volley.newRequestQueue(this)
+        val url = Global.urlWS + "tenant/tenantreviews"
+
+        var stringRequest = object : StringRequest(
+            Method.POST, url, Response.Listener {
+                Log.d("VOLLEY", it)
+                val obj = JSONObject(it)
+                if (obj.getString("status") == "success") {
+                    val data = obj.getJSONArray("data")
+                    for (i in 0 until data.length()) {
+                        var reviewObj = data.getJSONObject(i)
+                        val review = Review(reviewObj.getString("unit_no"), reviewObj.getInt("service_rating"), reviewObj.getString("service_review"))
+                        tenantReviews.add(review)
+                    }
+
+                    binding.btnTenReview.text = "Lihat Review Toko (" + tenantReviews.size + ")"
+                }
+                else if(obj.getString("status")=="empty"){
+                    binding.btnTenReview.isVisible = false
+                }
+                else if(obj.getString("status")=="notauthenticated"){
+                    Helper.logoutSystem(this)
+                }
+            },
+            Response.ErrorListener {
+                val builder = MaterialAlertDialogBuilder(this)
+                builder.setCancelable(false)
+                builder.setTitle("Terjadi Masalah")
+                builder.setMessage("Terdapat Masalah Jaringan\nSilakan Coba Lagi Nanti.")
+                builder.setPositiveButton("OK"){dialog, which->
+                }
+                builder.create().show()
+            }){
+            override fun getParams(): MutableMap<String, String> {
+                val params = HashMap<String, String>()
+                params["token"] = token
+                params["tenant_id"] = tenant_id.toString()
+                return params
+            }
+        }
+        stringRequest.setShouldCache(false)
+        q.add(stringRequest)
     }
 }
